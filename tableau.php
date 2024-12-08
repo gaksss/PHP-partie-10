dda066fc1cc9071003790f348c117ce788daca4d
gaksss <clement.carlier2@gmail.com>
2024-12-08 22:18:14 +0100

    DONE: TP3 partie 10

---
+++ tableau.php
@@ -0,0 +1,10 @@
+
+    <?php
+    $portrait1 = array('name'=>'Victor', 'firstname'=>'Hugo', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/5/5a/Bonnat_Hugo001z.jpg');
+
+
+foreach ($portrait1 as $key => $champ) {
+    echo $champ . "<br>";
+}
+
+var_dump($_POST);
